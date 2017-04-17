9d3acab1771ae998d35d659442455e851e9f6c71
James Dudley <james@dudley.codes>
2017-04-17 17:31:41 -0500

    Cleaned up edges

--- start-mcserver.bat
+++ start-mcserver.bat
@@ -1,12 +1,18 @@
 @ECHO OFF
 
-if exist .\minecraft_server.1.11.2.jar (
-    echo Starting Minecraft Server
-    java -Xmx1024M -Xms1024M -jar minecraft_server.1.11.2.jar nogui
+where java
+if %errorlevel% equ 1 (
+	echo "Java must be installed"
+	pause
 ) else (
-    echo Could not find Minecraft Server
-    echo download from https://minecraft.net/en/download/server
-    pause
+	if exist .\minecraft_server.1.11.2.jar (
+		echo Starting Minecraft Server
+		java -Xmx1024M -Xms1024M -jar minecraft_server.1.11.2.jar nogui
+	) else (
+		echo Could not find Minecraft Server
+		echo download from https://minecraft.net/en/download/server
+		pause
+	)
 )
 
 @ECHO ON
